59e81ee7882b1c639984b499d9d9b2d5a09d953b
James Pancoast <jpancoast@gmail.com>
2012-03-20 20:55:57 -0600

    Initial Commit

---
+++ Network-Discovery/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Network-Discovery
+//
+//  Created by James Pancoast on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
