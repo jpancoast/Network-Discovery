218b103772b0eb443a0d0a33f61da05bc22799e1
James Pancoast <jpancoast@gmail.com>
2012-03-20 21:32:22 -0600

    copied and pasted all the bonjour code in

--- Network-Discovery/bonjour-discovery.m
+++ Network-Discovery/bonjour-discovery.m
@@ -16,4 +16,133 @@
 }
 
 
+- (id)init
+{
+    NSLog( @"init" );
+    
+    self = [ super init ];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+
+//---resolve the IP address of a service---
+-(void) resolveIPAddress:(NSNetService *)service {    
+    NSNetService *remoteService = service;
+    remoteService.delegate = self;
+    [remoteService resolveWithTimeout:0];
+}
+
+
+//---browse for services---
+/*
+-(void) browseServices {
+    DevelopmentLog( @"Bonjour: browseServices" );
+    services = [NSMutableArray new];
+    self.browser = [[NSNetServiceBrowser alloc ] init ];
+    
+    self.browser.delegate = self;
+    
+    
+//     NSNetServiceBrowser
+//     searchForServicesOfType: @"_services._dns-sd._udp." 
+//     inDomain:                @""
+     
+    //    [self.browser searchForServicesOfType:@"_ssh._tcp." inDomain:@""];
+    [ self.browser searchForServicesOfType: @"_services._dns-sd._udp" inDomain: @"" ];  //This type is used to discover all services being advertised.  You then have to do the call again to see who's advertising it.
+}
+*/
+
+//---services found---
+/*
+ (if you want a specific domain then set domain appropriately) In the didFindService callback, only the name, type, and domain will be valid fields in the NSNetService. You then want to transform these three fields into a servicetype and servicedomain for feeding into another NSNetServiceBrowser to find all the actual services. For local services the three fields will be of the form:
+ 
+ http://fernlightning.com/doku.php?id=randd:bonjour
+ */
+/*
+-(void)netServiceBrowser:(NSNetServiceBrowser *)aBrowser
+          didFindService:(NSNetService *)aService
+              moreComing:(BOOL)more {
+    [services addObject:aService];
+    
+    DevelopmentLog( @"Bonjour: Found Service.  Resolving... Name: %@, type: %@, domain: %@", [ aService name ], [aService type ], [aService domain ] );
+    
+    NSString *tempString = [ NSString stringWithFormat: @"Bonjour: Found Service.  Resolving... Name: %@, type: %@, domain: %@", [ aService name ], [aService type ], [aService domain ] ];
+    
+    
+    //self.listOfDevices = [ self.listOfDevices stringByAppendingString: tempString ];
+    //self.listOfDevices = [ NSString stringWithFormat: @"%@\n%@", self.listOfDevices, tempString ];
+    if ( self.listOfDevices == nil )
+        self.listOfDevices = tempString;
+    else
+        self.listOfDevices = [ NSString stringWithFormat: @"%@\n%@", self.listOfDevices, tempString ];
+    
+    
+    DevelopmentLog( @"Blah: %@", self.listOfDevices );
+    
+    //    [self resolveIPAddress:aService];//Commented out because the call is just getting the services being advertised, so there isn't an ip address.  If we search for a specific service, like _ssh., then we should be able to do this call.
+}
+*/
+
+
+//---services removed from the network---
+/*
+-(void)netServiceBrowser:(NSNetServiceBrowser *)aBrowser
+        didRemoveService:(NSNetService *)aService
+              moreComing:(BOOL)more {
+    [services removeObject:aService];
+    
+    DevelopmentLog( @"Bonjour: Removed: %@", [aService hostName] );
+}
+*/
+
+
+//---managed to resolve---
+/*
+-(void)netServiceDidResolveAddress:(NSNetService *)service {
+    NSString           *name = nil;
+    NSData *address = nil;
+    
+    struct sockaddr_in *socketAddress = nil;
+    
+    NSString *ipString = nil;
+    
+    int port;
+    
+    
+    for(int i=0;i < [[service addresses] count]; i++ )
+    {
+        name = [service name];
+        address = [[service addresses] objectAtIndex: i];
+        socketAddress = (struct sockaddr_in *) [address
+                                                bytes];
+        ipString = [NSString stringWithFormat: @"%s", inet_ntoa(socketAddress->sin_addr)];
+        
+        port = socketAddress->sin_port;
+        
+        DevelopmentLog( @"Bonjour: Resolved: %@-->%@:%hu\n", [service hostName], ipString, port );
+    }
+    
+}
+*/
+
+
+/*
+- (void)netService:(NSNetService *)sender didUpdateTXTRecordData:(NSData *)data
+{
+    DevelopmentLog( @"Bonjour: we're getting back txt record data, wonder what it is?" );
+}
+*/
+
+
+//---did not managed to resolve---
+/*
+-(void)netService:(NSNetService *)service didNotResolve:(NSDictionary *)errorDict {
+    DevelopmentLog( @"Bonjour: Could Not Resolve: %@", errorDict );
+}
+ */
+
 @end
